25b61d7e6450c93d978fa221110e1d92e4d89371
Miliça Cerovski <cerovskimilica@gmail.com>
2026-03-11 14:01:48 +0000

    commit

--- models/intermediate/sub_exchange.sql
+++ models/intermediate/sub_exchange.sql
@@ -1,30 +1,34 @@
 WITH pro AS (
-  SELECT
-    s.* EXCEPT(country),
-    e.* EXCEPT(country),
-    s.country AS country_sub,
-    e.country AS country_exc,
-    s.* EXCEPT(region),
-    e.* EXCEPT(region),
-    s.region AS region_sub,
-    e.region AS region_exc,
-    s.* EXCEPT(city),
-    e.* EXCEPT(city),
-    s.city AS city_sub,
-    e.city AS city_exc,
-    s.* EXCEPT(department),
-    e.* EXCEPT(department),
-    s.department AS department_sub,
-    e.department AS department_exc,
-  FROM {{ ref('stg_raw_data__exchanges') }} AS s
-  LEFT JOIN {{ ref('sub_no_duplicates') }} AS e
-    ON e.user_id = s.host_user_id
+  SELECT
+    s.* EXCEPT(country, region, city, department),
+    e.* EXCEPT(country, region, city, department),
+
+    s.country AS country_exc,
+    e.country AS country_sub,
+
+    s.region AS region_exc,
+    e.region AS region_sub,
+
+    s.city AS city_exc,
+    e.city AS city_sub,
+
+    s.department AS department_exc,
+    e.department AS department_sub
+
+  FROM {{ ref('stg_raw_data__exchanges') }} AS s
+  LEFT JOIN {{ ref('sub_no_duplicates') }} AS e
+    ON e.user_id = s.host_user_id
 )
 
 SELECT
-  pro.* EXCEPT(country_sub, country_exc),
-  COALESCE(country_sub, country_exc) AS country
-  COALESCE(region_sub, region_exc) AS region
-  COALESCE(city_sub, city_exc) AS city
-  COALESCE(department_sub, department_exc) AS department
+  pro.* EXCEPT(
+    country_exc, country_sub,
+    region_exc, region_sub,
+    city_exc, city_sub,
+    department_exc, department_sub
+  ),
+  COALESCE(country_exc, country_sub) AS country,
+  COALESCE(region_exc, region_sub) AS region,
+  COALESCE(city_exc, city_sub) AS city,
+  COALESCE(department_exc, department_sub) AS department
 FROM pro
