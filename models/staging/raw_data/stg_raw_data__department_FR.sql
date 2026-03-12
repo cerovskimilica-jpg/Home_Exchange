9cf11ae41ffe3f0784dea003eeda48120cd26c4e
Andrea ENGELKE <andreaengelke99@gmail.com>
2026-03-12 08:35:15 +0000

    Staging

--- models/staging/raw_data/stg_raw_data__department_FR.sql
+++ models/staging/raw_data/stg_raw_data__department_FR.sql
@@ -1,22 +1,15 @@
-with 
-
-source as (
-
-    select * from {{ source('raw_data', 'department_FR') }}
-
-),
-
-renamed as (
-
-    select
-        country,
-        department_code,
-        department_isocode,
-        department_name,
-        region_name
-
-    from source
-
-)
-
-select * from renamed
+SELECT
+  * EXCEPT(department_name),
+  CASE
+    WHEN department_name = 'Tarn-et-Garonne' THEN 'Tarn-Et-Garonne'
+    WHEN department_name = 'Territoire de Belfort' THEN 'Territoire De Belfort'
+    WHEN department_name = 'Val-d’Oise' THEN "Val-D'oise"
+    WHEN department_name = 'Val-de-Marne' THEN 'Val-De-Marne'
+    WHEN department_name = 'Hauts-de-Seine' THEN 'Hauts-De-Seine'
+    WHEN department_name = 'Seine-et-Marne' THEN 'Seine-Et-Marne'
+    WHEN department_name = 'Maine-et-Loire' THEN 'Maine-Et-Loire'
+    WHEN department_name = 'Bouches-du-Rhône' THEN 'Bouches-Du-Rhône'
+    WHEN department_name = 'Alpes-de-Haute-Provence' THEN 'Alpes-De-Haute-Provence'
+    ELSE department_name
+  END AS department_clean
+FROM {{ ref('stg_raw_data__department_FR') }}
