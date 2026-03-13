cefb70a18bf2199d7341faa4e9e608b9778dcca9
Franck BREBION <francklaurentbrebion@gmail.com>
2026-03-13 14:47:31 +0000

    commit

--- models/intermediate/sub_exchanges_adresses_HostisocodeFR.sql
+++ models/intermediate/sub_exchanges_adresses_HostisocodeFR.sql
@@ -1,37 +1,9 @@
-WITH subadresse AS (
-  SELECT
-    se.*, 
-    ad.* EXCEPT(country_host, region_host, city_host, department_host, host_user_id, last_subscription_date, residence_type),
-  
-  -- JOIN la table adresse et sub_exchange sur la base du guest ID (où l'on cherche l'adresse) auquel on renvoie l'adresse du host_ID provenant de la table Adresse
-    ad.country_host as country_guest2,
-    ad.region_host as region_guest2,
-    ad.department_host as department_guest2,
-    ad.city_host as city_guest2
+-- Objectif: rajouter les isocodes FR pour les department_host à la table sub_exchanges_adresses
+-- et d'ajouter via le case when les ISO code pour les department_host qui avaient des valeurs erronées
 
-      FROM {{ ref('sub_exchange') }} AS se
-        LEFT JOIN {{ ref('adresse_host') }} AS ad
-        ON se.guest_user_id = ad.host_user_id  
-),
-
-antoine AS (
-SELECT
-  subadresse.* EXCEPT(
-    country_guest, department_guest, region_guest, city_guest,
-    country_guest2, department_guest2, region_guest2, city_guest2
-  ),
-
-  COALESCE(country_guest, country_guest2) AS country_guestdf, -- remplacement des valeurs nulles d'adresse de guest par leur adresse en tant que Host si disponible
-  COALESCE(region_guest, region_guest2) AS region_guestdf,
-  COALESCE(department_guest, department_guest2) AS department_guestdf,
-  COALESCE(city_guest, city_guest2) AS city_guestdf
-
-FROM subadresse
-),
-
-jointure AS (
+WITH jointure AS (
   SELECT *
-  FROM antoine AS exc
+  FROM {{ ref('sub_exchanges_adresses') }} AS exc
   LEFT JOIN {{ ref('stg_raw_data__department_FR') }} AS dep
   ON exc.department_host = dep.department_clean
 ),
