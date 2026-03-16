33b6fadf3124d89c080e8da3c8a132c269e4b445
Franck BREBION <francklaurentbrebion@gmail.com>
2026-03-16 20:08:41 +0000

    OK

--- models/mart/churn_referal_bytenure.sql
+++ models/mart/churn_referal_bytenure.sql
@@ -1,19 +1,23 @@
 -- Analyser le churn selon l’ancienneté (tenure) est le beaucoup plus informatif
--- Il évite le bias que les nouveaux entrants churnent plus que les anciens et qu'on a generalement bcp de nouveaux.
-
-SELECT
-  CASE
-    WHEN DATE_DIFF(subscription_date, first_subscription_date, YEAR) = 0 THEN 'Year 1'
-    WHEN DATE_DIFF(subscription_date, first_subscription_date, YEAR) = 1 THEN 'Year 2'
-    WHEN DATE_DIFF(subscription_date, first_subscription_date, YEAR) = 2 THEN 'Year 3'
-    WHEN DATE_DIFF(subscription_date, first_subscription_date, YEAR) = 3 THEN 'Year 4'
-    ELSE 'Year 5+'
-  END AS tenure_bucket,
-  referral,
-  COUNT(*) AS users,
-  COUNTIF(renew = 0) AS churners,
-  SAFE_DIVIDE(COUNTIF(renew = 0), COUNT(*)) AS churn_rate
-FROM {{ ref('stg_raw_data_subscriptions_category_clean') }}
-WHERE renew IS NOT NULL
-GROUP BY tenure_bucket, referral
-ORDER BY tenure_bucket;
+-- Il évite le bias que les nouveaux entrants churnent plus que les anciens et qu'on a
+-- generalement bcp de nouveaux.
+select
+    case
+        when date_diff(subscription_date, first_subscription_date, year) = 0
+        then 'Year 1'
+        when date_diff(subscription_date, first_subscription_date, year) = 1
+        then 'Year 2'
+        when date_diff(subscription_date, first_subscription_date, year) = 2
+        then 'Year 3'
+        when date_diff(subscription_date, first_subscription_date, year) = 3
+        then 'Year 4'
+        else 'Year 5+'
+    end as tenure_bucket,
+    referral,
+    count(*) as users,
+    countif(renew = 0) as churners,
+    safe_divide(countif(renew = 0), count(*)) as churn_rate
+from {{ ref("stg_raw_data_subscriptions_category_clean") }}
+where renew is not null
+group by tenure_bucket, referral
+order by tenure_bucket
